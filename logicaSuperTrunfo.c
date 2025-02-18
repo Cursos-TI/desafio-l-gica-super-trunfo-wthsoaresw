6b5400136834d3a0ce90245d2e9f60782fc7ebf1
wthsoaresw <thiagosoares72474877@gmail.com>
2025-02-18 00:26:12 +0000

    crie while

--- logicaSuperTrunfo.c
+++ logicaSuperTrunfo.c
@@ -1,54 +1,22 @@
-#include <stdio.h>
+int main() {
+    int i = 0;
 
-// Desafio Super Trunfo - Países
-// Tema 2 - Comparação das Cartas
-    
-// Este código inicial serve como base para o desenvolvimento do sistema de comparação de cartas de cidades. 
-    // Siga os comentários para implementar cada parte do desafio.
-    // Definição das variáveis para armazenar as propriedades das cidades
-    // Você pode utilizar o código do primeiro desafio
+    while (i <= 10);{
 
-    
-    // Cadastro das Cartas:
-    // Implemente a lógica para solicitar ao usuário que insira os dados das cidades
-    // utilizando a função scanf para capturar as entradas.
-    // utilize o código do primeiro desafio
+        
+        if(i % 2 == 0)
+       
+        {
+           printf("o numero %d é par! \n, i");
+        }
+        
+        i++;
 
-    // Exemplo:
-    // printf("Digite o código da cidade: ");
-    // scanf("%s", codigo);
-    // 
-    // (Repita para cada propriedade)
 
-    // Comparação de Cartas:
-    // Desenvolva a lógica de comparação entre duas cartas.
-    // Utilize estruturas de decisão como if, if-else para comparar atributos como população, área, PIB, etc.
-
-    // Exemplo:
-    // if (populacaoA > populacaoB) {
-    //     printf("Cidade 1 tem maior população.\n");
-    // } else {
-    //     printf("Cidade 2 tem maior população.\n");
-    // }
-
-    // Exibição dos Resultados:
-    // Após realizar as comparações, exiba os resultados para o usuário.
-    // Certifique-se de que o sistema mostre claramente qual carta venceu e com base em qual atributo.
-
-    // Exemplo:
-    // printf("A cidade vencedora é: %s\n", cidadeVencedora);
-    
-int main (){
-    printf("desafio Super Trunfo\n");
-    printf("novo conmit\n");
-    int cartaA01, cartaA02;
-
-    cartaA01 = 699.26;
-    cartaA02 = 700.30;
-
-    if (cartaA01 >= cartaA02) {
-        printf("numeroA01 é maior que o numeroA02");
     }
-     printf("fora do if\n");
-
+    
+    
+    
+    
+    return 0;
 }
